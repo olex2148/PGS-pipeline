6e88134b26bf29df5c6185e5dcb0f9a66f122a44
Ole <30407920+olex2148@users.noreply.github.com>
2024-01-22 15:02:43 +0100

    Moved modpath

--- workflow.py
+++ workflow.py
@@ -18,31 +18,11 @@ Todo:
 """
 
 from gwf import Workflow, AnonymousTarget
-import os, re, glob
+import glob
 from datetime import date
+from code.modpath import modpath
+
 gwf = Workflow()
-
-### Helper functions
-def modpath(p, parent=None, base=None, suffix=None):
-	'''
-	A function to split path strings into path, basename and suffix
-	for easy manipulation of input and output names in the workflow.
-	'''
-	par, name = os.path.split(p)
-	name_no_suffix, suf = os.path.splitext(name)
-	if type(suffix) is str:
-		suf = suffix
-	if parent is not None:
-		par = parent
-	if base is not None:
-		name_no_suffix = base
-
-	new_path = os.path.join(par, name_no_suffix + suf)
-	if type(suffix) is tuple:
-		assert len(suffix) == 2
-		new_path, nsubs = re.subn(r'{}$'.format(suffix[0]), suffix[1], new_path)
-		assert nsubs == 1, nsubs
-	return new_path
 
 ### Defining gwf templates
 def parse_sumstats(inputfile):
