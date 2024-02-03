f0417cfbe9744cde1d437899c858da7836965fab
olex2148 <hamolehansen@gmail.com>
2024-02-03 11:18:24 +0100

    snp_match before QC

--- sandbox/munge_sumstats.R
+++ sandbox/munge_sumstats.R
@@ -9,14 +9,14 @@
 #'   - Prioritize Neff_half or 4/(1/cases + 1/controls)?
 
 
-if (!require("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-BiocManager::install(version = "3.18") # Version needed for MungeSumstats
-BiocManager::install("MungeSumstats")
-
-# Installing ref genomes
-BiocManager::install("SNPlocs.Hsapiens.dbSNP144.GRCh37", "BSgenome.Hsapiens.1000genomes.hs37d5",
-                     "SNPlocs.Hsapiens.dbSNP144.GRCh38", "BSgenome.Hsapiens.NCBI.GRCh38")
+# if (!require("BiocManager", quietly = TRUE))
+#   install.packages("BiocManager")
+# BiocManager::install(version = "3.18") # Version needed for MungeSumstats
+# BiocManager::install("MungeSumstats")
+# 
+# # Installing ref genomes
+# BiocManager::install("SNPlocs.Hsapiens.dbSNP144.GRCh37", "BSgenome.Hsapiens.1000genomes.hs37d5",
+#                      "SNPlocs.Hsapiens.dbSNP144.GRCh38", "BSgenome.Hsapiens.NCBI.GRCh38")
 
 suppressPackageStartupMessages({
   library(dplyr)
@@ -35,15 +35,15 @@ output = paste0("steps/munged_sumstats/", base_name, "_munged.rds")
 
 source("code/aux/input_paths.R")
 
-# sumstats = read_sumstats("data/daner_bip_pgc3_nm_noukbiobank") # For testing
+# sumstats = read_sumstats(test_path) # For testing
 head(sumstats)
 
-# Inferring ref genome
-sumstats_list <- list(ss1 = sumstats) # Because "get_genome_build" doesn't exist
-ref_genome <- get_genome_builds(sumstats_list = sumstats_list, dbSNP = 144)$ss1     # ~2 mins
-
 # Formatting sumstats using MungeSumstats ---------------------------------------------------------------------------------
-reformatted <- format_sumstats(path=sumstats,                                       # ~10 mins
+# Inferring ref genome
+# list because "get_genome_build" doesn't exist
+ref_genome <- get_genome_builds(sumstats_list = list(ss1 = sumstats), dbSNP = 144)$ss1  # ~2 mins
+
+reformatted <- format_sumstats(path=sumstats,                                           # ~10 mins
                                ref_genome=ref_genome, dbSNP = 144,                       # Detected ref genome
                                convert_ref_genome = "GRCh37",                            # Convert to HapMap3+ build if not already GRCh37
                                impute_beta = TRUE, impute_se = TRUE,                     # Convert OR to beta and se to beta_se
@@ -54,24 +54,46 @@ reformatted <- format_sumstats(path=sumstats,                                   
           rename(POS = BP, A0 = A1, A1 = A2, BETA_SE = SE) %>%                           # Renaming to fit LDpred2 format
           mutate(CHR = ifelse(CHR == "X", 23, ifelse(CHR == "Y", 24, as.numeric(CHR))))  # Converting X and Y chr to 23 and 24 
 
+# Finding Hapmap and iPSYCH overlap with sumstats ----------------------------------------------------------------------------
+
+# Reading in HapMap3+ 
+info <- readRDS(runonce::download_file(
+  "https://figshare.com/ndownloader/files/37802721",
+  dir = hapmap_path, fname = "map_hm3_plus.rds"))
+
+# Reading in iPSYCH data
+dosage <- snp_attach(dosage_path) 
+dosage$map <- dosage$map %>% 
+  rename("chr" = "CHR", "pos" = "POS", "a0" = "a1", "a1" = "a2")
+
+# Making colnames lower case for snp_match
+colnames(reformatted) <- tolower(colnames(reformatted)) 
+
+# Finding sumstats/iPSYCH overlap
+snp_info <- snp_match(reformatted, dosage$map)                                                                                      
+
+# Finding HapMap3+ overlap
+in_test <- vctrs::vec_in(snp_info[, c("chr", "pos")], info[, c("chr", "pos")])
+snp_info <- snp_info[in_test, ]                          
+
 # Some manual checks -------------------------------------------------------------------------------------------------------
 
 # Odds ratio -------------------------------------------------
 
 # If reported effect size is odds ratio, MungeSumstats 
 # does not convert se, as long as SE exists
-if("OR" %in% colnames(reformatted)){
-  reformatted$BETA_SE = reformatted$BETA/qnorm(1-reformatted$P / 2) # beta/z
+if("or" %in% colnames(snp_info)){
+  snp_info$beta_se = snp_info$beta/qnorm((1-snp_info$p) / 2) # beta/z
 }
 
 # Effective population size ----------------------------------
 
-if(!"N_EFF" %in% colnames(reformatted)){
-  if("N_CAS" %in% colnames(reformatted)){
-    reformatted$N_EFF = 4/(1/reformatted$N_CAS + 1/reformatted$N_CON)
+if(!"n_eff" %in% colnames(snp_info)){
+  if("n_cas" %in% colnames(snp_info)){
+    snp_info$n_eff = 4/(1/snp_info$n_cas + 1/snp_info$n_con)
   } else {
-      if("NEFF_HALF" %in% colnames(reformatted)){
-        reformatted$N_EFF = reformatted$NEFF_HALF * 2
+      if("neff_half" %in% colnames(snp_info)){
+        snp_info$n_eff = snp_info$neff_half * 2
       }
   }
 }
@@ -79,31 +101,31 @@ if(!"N_EFF" %in% colnames(reformatted)){
 # Making sure its in the sumstats 
 # - otherwise should be added manually
 assert("No effective population size in parsed sumstats",
-       "N_EFF" %in% colnames(reformatted))
+       "n_eff" %in% colnames(snp_info))
 
 # Allele frequency ------------------------------------------
 
 # Check if frq columns are on the form frq_a_X and frq_u_X
-colnames(reformatted)[grep("^FR?Q_A_", colnames(reformatted))] <- "FRQ_CAS"
-colnames(reformatted)[grep("^FR?Q_U_", colnames(reformatted))] <- "FRQ_CON"
+colnames(snp_info)[grep("^fr?q_a_", colnames(snp_info))] <- "frq_cas"
+colnames(snp_info)[grep("^fr?q_u_", colnames(snp_info))] <- "frq_con"
 
-if(!"FRQ" %in% colnames(reformatted)){
-  if("FRQ_CAS" %in% colnames(reformatted)) {
-    reformatted$FRQ = (reformatted$FRQ_CAS + reformatted$FRQ_CON)/2
+if(!"frq" %in% colnames(snp_info)){
+  if("frq_cas" %in% colnames(snp_info)) {
+    snp_info$frq = (snp_info$frq_cas + snp_info$frq_con)/2
   }
 }
 
 # QC ------------------------------------------------------------------------------------------------------------------
 
 # Beta, beta_se, and n_eff  ---------------------------------------
-parsed_sumstats <- reformatted %>% 
-  filter(BETA != 0, BETA_SE > 0 & N_EFF > (0.7 * max(N_EFF)))
+df_beta <- snp_info %>% 
+  filter(beta != 0 & beta_se > 0 & n_eff > (0.7 * max(n_eff)))
 
 # Allele frequency -----------------------------------------------
-if("FRQ" %in% colnames(parsed_sumstats)){         # If freq exists
+if("frq" %in% colnames(df_beta)){         # If freq exists
   
-  sd_af <- with(parsed_sumstats, sqrt(2 * FRQ * (1 - FRQ)))
-  sd_ss <- with(parsed_sumstats, 2 / sqrt(N_EFF * BETA_SE^2 + BETA^2))
+  sd_af <- with(df_beta, sqrt(2 * frq * (1 - frq)))
+  sd_ss <- with(df_beta, 2 / sqrt(n_eff * beta_se^2 + beta^2))
   sd_ss2 <- sd_ss / quantile(sd_ss, 0.999) * sqrt(0.5) 
   
   is_bad <- 
@@ -123,42 +145,20 @@ if("FRQ" %in% colnames(parsed_sumstats)){         # If freq exists
   
   ggsave(paste0("results/", base_name, "/", base_name, "_QC.jpeg"), p)
   
-  parsed_sumstats <- parsed_sumstats[!is_bad, ] 
+  df_beta <- df_beta[!is_bad, ] 
   
 } else {
   cat("No allele frequencies available in summary statistics. QC step not performed. \n")
 }
 
-
-# Finding Hapmap and iPSYCH overlap with sumstats ----------------------------------------------------------------------------
-
-# Reading in HapMap3+ 
-info <- readRDS(runonce::download_file(
-  "https://figshare.com/ndownloader/files/37802721",
-  dir = hapmap_path, fname = "map_hm3_plus.rds"))
-
-# Reading in iPSYCH data
-dosage <- snp_attach(dosage_path) %>% 
-  renmame("chr" = "CHR", "pos" = "POS", "a0" = "a1", "a1" = "a2")
-
-# Making colnames lower case for snp_match
-colnames(parsed_sumstats) <- tolower(colnames(parsed_sumstats)) 
-
-# Finding sumstats/iPSYCH overlap
-df_beta <- snp_match(parsed_sumstats, dosage$map)                                                                                      
-
-# Finding HapMap3+ overlaps
-in_test <- vctrs::vec_in(df_beta[, c("chr", "pos")], info[, c("chr", "pos")])
-df_beta <- df_beta[in_test, ]                                                                                      
-
-# Making sure there are at least 500K variants in sumstats
+# Making sure there are at least 500K variants in sumstats -----------------------------------------------------------------
 assert("Less than 500K variants remaining in summary statistics following QC and Hapmap3+/iPSYCH overlap.", 
-       nrow(parsed_sumstats) > 500000)
-cat(nrow(parsed_sumstats), "variants remaining in munged sumstats.")
+       nrow(df_beta) > 500000)
+cat(nrow(df_beta), "variants remaining in munged sumstats.")
 
 # Saving the parsed sumstats in the outputfile ------------------------------------------------------------------------------
 
-head(parsed_sumstats)
-saveRDS(parsed_sumstats, output)
+head(df_beta)
+saveRDS(df_beta, output)
 
-# saveRDS(parsed_sumstats, "steps/parsed_sumstats/test_adhd.rds") # for testing
+# saveRDS(df_beta, "steps/munged_sumstats/test_adhd.rds") # for testing
