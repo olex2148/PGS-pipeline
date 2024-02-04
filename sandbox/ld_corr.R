185caf4c2f45779b6391d6a3923b1c04de4c3245
olex2148 <hamolehansen@gmail.com>
2024-02-04 10:40:14 +0100

    Reading in full HapMap3+ and saving

--- sandbox/ld_corr.R
+++ sandbox/ld_corr.R
@@ -1,18 +1,10 @@
-tmp <- tempfile(tmpdir = "steps/corr")
+tmp <- tempfile(tmpdir = "data/corr", pattern = "hm3+_corr")
 
-cat("Reading in LD blocks for chromosome ")
 for (chr in 1:22) {
   
   cat(chr, "..", sep = "")
   
-  ## indices in df_beta
-  ind.chr <- which(df_beta$chr == chr)
-  ## indices in map_ldref
-  ind.chr2 <- df_beta$`_NUM_ID_`[ind.chr]
-  ## indices in corr_chr
-  ind.chr3 <- match(ind.chr2, which(info$chr == chr))
-  
-  corr_chr <- readRDS(paste0(ld_blocks, chr, ".rds"))[ind.chr3, ind.chr3]
+  corr_chr <- readRDS(paste0(ld_blocks, chr, ".rds"))
   
   if (chr == 1) {
     corr <- as_SFBM(corr_chr, tmp, compact = TRUE)
@@ -20,4 +12,25 @@ for (chr in 1:22) {
     corr$add_columns(corr_chr, nrow(corr))
   }
 }
-cat("\n")
+corr$save()
+
+# cat("Reading in LD blocks for chromosome ")
+# for (chr in 1:22) {
+#   
+#   cat(chr, "..", sep = "")
+#   
+#   ## indices in df_beta
+#   ind.chr <- which(df_beta$chr == chr)
+#   ## indices in map_ldref
+#   ind.chr2 <- df_beta$`_NUM_ID_`[ind.chr]
+#   ## indices in corr_chr
+#   ind.chr3 <- match(ind.chr2, which(info$chr == chr))
+#   
+#   corr_chr <- readRDS(paste0(ld_blocks, chr, ".rds"))[ind.chr3, ind.chr3]
+#   
+#   if (chr == 1) {
+#     corr <- as_SFBM(corr_chr, tmp, compact = TRUE)
+#   } else {
+#     corr$add_columns(corr_chr, nrow(corr))
+#   }
+# }
