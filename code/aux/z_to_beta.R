66c039acd3da30d71cbb4c1dbf0ba368b594929a
olex2148 <hamolehansen@hotmail.com>
2024-02-25 11:26:29 +0100

    Added beta_se

--- code/aux/z_to_beta.R
+++ code/aux/z_to_beta.R
@@ -4,13 +4,16 @@
 #' 
 
 z_to_beta <- function(sumstats){
-  
-  if(all(c("z", "p", "n")) %in% colnames(sumstats)){
-    cat("Deriving BETA from Z, N, and P")
-    sumstats$beta <- with(sumstats, z/sqrt(qchisq(p, n)))
-
-  } else if (all(c("z", "n", "frq")) %in% colnames(sumstats)){
-    cat("Deriving BETA from Z, N, and FRQ")
-    sumstats$beta <- with(sumstats, z/sqrt(2 * frq * (1 - frq) * (n + z^2)))
+  if("z" %in% colnames(sumstats) & !"beta" %in% colnames(sumstats)) {
+    if(all(c("z", "p", "n")) %in% colnames(sumstats)){
+      cat("Deriving BETA from Z, N, and P")
+      sumstats$beta <- with(sumstats, z/sqrt(qchisq(p, n)))
+      
+    } else if (all(c("z", "n", "frq")) %in% colnames(sumstats)){
+      cat("Deriving BETA from Z, N, and FRQ")
+      sumstats$beta <- with(sumstats, z/sqrt(2 * frq * (1 - frq) * (n + z^2)))
+    }
+    
+    sumstats$beta_se <- with(sumstats, abs(beta) / qnorm(pmax(p, .Machine$double.xmin) / 2, lower.tail = FALSE)) # beta/z
   }
 }
