32f88c402dc996ce83c321c84ecc9f9036212b32
olex2148 <hamolehansen@hotmail.com>
2024-02-25 13:21:03 +0100

    Moved )

--- code/aux/z_to_beta.R
+++ code/aux/z_to_beta.R
@@ -5,15 +5,16 @@
 
 z_to_beta <- function(sumstats){
   if("z" %in% colnames(sumstats) & !"beta" %in% colnames(sumstats)) {
-    if(all(c("z", "p", "n")) %in% colnames(sumstats)){
+    if(all(c("z", "p", "n") %in% colnames(sumstats))){
       cat("Deriving BETA from Z, N, and P")
       sumstats$beta <- with(sumstats, z/sqrt(qchisq(p, n)))
       
-    } else if (all(c("z", "n", "frq")) %in% colnames(sumstats)){
+    } else if (all(c("z", "n", "frq") %in% colnames(sumstats))){
       cat("Deriving BETA from Z, N, and FRQ")
       sumstats$beta <- with(sumstats, z/sqrt(2 * frq * (1 - frq) * (n + z^2)))
     }
     
     sumstats$beta_se <- with(sumstats, abs(beta) / qnorm(pmax(p, .Machine$double.xmin) / 2, lower.tail = FALSE)) # beta/z
   }
+  return(sumstats)
 }
