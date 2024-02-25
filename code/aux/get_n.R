557e5134b8c061681ebf1cac29b42bcf2972364d
olex2148 <hamolehansen@hotmail.com>
2024-02-25 13:53:10 +0100

    Updated get_n

--- code/aux/get_n.R
+++ code/aux/get_n.R
@@ -10,27 +10,23 @@ get_n_cas_con <- function(initial_sample_size_str) {
   # Split the string into parts
   parts <- unlist(strsplit(sample_size_str, ", "))
   
-  # If theres only one sentence in this variable, continuous trait and will return only n
-  if(length(parts) == 1) {
-    n <- as.numeric(gsub("[^0-9]", "", parts))
-    
-    return(list("n" = n, "n_cas" = NA, "n_con" = NA))  
-           
-  } else { 
-  # Otherwise return ncas ncon
-    
-    # Initialize
-    sum_cases <- 0
-    sum_controls <- 0
-    
-    # Loop through the parts and sum cases and controls separately
-    for (part in parts) {
-      if (grepl("cases$", part)) {
-        sum_cases <- sum_cases + as.numeric(gsub("[^0-9]", "", part))
-      } else if (grepl("controls$", part)) {
-        sum_controls <- sum_controls + as.numeric(gsub("[^0-9]", "", part))
-      }
+  # Initialize
+  sum_cases <- sum_controls <- sum_inds <- 0
+  
+  # Loop through the parts and sum cases and controls separately
+  for (part in parts) {
+    if(grepl("cases$", part)) {
+      sum_cases <- sum_cases + as.numeric(gsub("[^0-9]", "", part))
+    } else if(grepl("controls$", part)) {
+      sum_controls <- sum_controls + as.numeric(gsub("[^0-9]", "", part))
+    } else if(grepl("individuals$", part)) {
+      sum_inds <- sum_inds + as.numeric(gsub("[^0-9]", "", part))
     }
-    return(list("n" = NA, "n_cas" = sum_cases, "n_con" = sum_controls))
   }
+  n_cas <- ifelse(sum_cases == 0, NA, sum_cases)
+  n_con <- ifelse(sum_controls == 0, NA, sum_controls)
+  n <- ifelse(sum_inds == 0, NA, sum_inds)
+  
+  return(list("n" = n, "n_cas" = n_cas, "n_con" = n_con))
+
 }
